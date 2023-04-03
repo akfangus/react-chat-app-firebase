f6c721f70fb594e381cd90cb7febacd299f7cf55
akfangus <akfanakfan@naver.com>
2023-04-04 01:25:44 +0900

    망했어

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -1,5 +1,18 @@
 import React from "react";
 
 export default function Chats() {
-  return <div></div>;
+  return (
+    <div className="chats">
+      <div className="userChat">
+        <img
+          src="https://images.unsplash.com/photo-1628813640706-ca88bcdfb228?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y2hlbHNlYXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"
+          alt=""
+        />
+        <div className="userChatInfo">
+          <span>Jane</span>
+          <p>Hello</p>
+        </div>
+      </div>
+    </div>
+  );
 }
